1ad037ebfc218f6bcea80dfb6cc5ec63aa06c39f
wangshihao <1065206798@qq.com>
2018-12-26 14:57:35 +0800

    第四次文件更新   2018年12月26日14:57:23   提交人：w

--- app/src/main/java/com/bawei/gitcreateandmerge/SettingActivity.java
+++ app/src/main/java/com/bawei/gitcreateandmerge/SettingActivity.java
@@ -3,4 +3,6 @@ package com.bawei.gitcreateandmerge;
 import android.support.v7.app.AppCompatActivity;
 
 public class SettingActivity extends AppCompatActivity {
+    //修改代码
+    // 2018年12月26日14:57:01
 }
